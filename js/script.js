0c4cc152e530e6986dc0d834184d2f7d6f8681a0
LautaroFuente <lautaro.fuente@yahoo.com>
2023-12-12 05:25:22 -0300

    fin

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
 
+const botones = document.querySelectorAll('.btn-js');
 
+const display = document.getElementById('display');
+
+const botonBorrar = document.getElementById('delete');
+
+const botonIgual = document.getElementById('equal');
+
+let expresion = '';
+
+botones.forEach(botón => {
+    botón.addEventListener('click', function() {
+
+        const textoBotón = this.innerText;
+
+        display.innerText += textoBotón;
+
+        expresion += textoBotón;
+    });
+});
+
+botonBorrar.addEventListener('click', function(){
+    display.innerText = '';
+    expresion = '';
+})
+
+function calcularExpresion() {
+    try {
+        const resultado = eval(expresion);
+        display.innerText = resultado;
+    } catch (error) {
+        display.innerText = 'Error';
+    }
+    finally{
+        expresion = '';
+    }
+}
+
+botonIgual.addEventListener('click', () =>{
+    calcularExpresion();
+});
